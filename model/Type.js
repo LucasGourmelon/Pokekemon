eb3c201b39806ad907fd37448bf111cbe1688086
LucasGourmelon <lucasgourmelon12@gmail.com>
2024-03-12 09:30:59 +0100

    Premier commit classe Type

--- model/Type.js
+++ model/Type.js
@@ -2,5 +2,20 @@ import "../files/pokemon_type.js"
 import "../files/type_effectiveness.js"
 
 class Type {
-    
+    constructor(type_attack, type_defense){
+        this.type_attack = type_attack;
+        this.type_defense = type_defense;        
+    }
+
+    getEffectiveness(){
+        return type_effectiveness[this.type_attack][this.type_defense];
+    }
+
+    type_effectiveness(){
+        return type_effectiveness;
+    }
+
+    toString(){
+        return `Type: ${this.type_attack} - ${this.type_defense}`;
+    }
 }
