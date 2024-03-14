bd5326b83557a51e83ede6979f9d4e67c22915b0
Yoh2929 <yohannmear29@gmail.com>
2024-03-14 09:05:26 +0100

    Commit fonction filtrer normal

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+import {Pokemon} from "./modele/Pokemon.js";
+
 $(document).ready(function(){
     console.log("Document pret");
+
+    console.log(Pokemon.getNormalPokemons());
 })
--- modele/Pokemon.js
+++ modele/Pokemon.js
@@ -1,6 +1,8 @@
 import "../files/pokemon.js";
 
-class Pokemon {
+
+export class Pokemon {
+    static allPokemons = {};
     constructor(pokemon_id, pokemon_name, form, base_attack, base_defense, base_stamina) {
         this.pokemon_id = pokemon_id;
         this.pokemon_name = pokemon_name;
@@ -10,34 +12,52 @@ class Pokemon {
         this.base_stamina = base_stamina;
     }
 
-    import_pokemon(donnees_pokemon){
-        let allPokemons = {};
-        let clesPokemon = donnees_pokemon.pokemon_id;
-        allPokemons.push(clesPokemon);
+    import_pokemon(){
     }
+    
+    static getNormalPokemons(sourceDonnees){
+        let donneesFiltreesPokemon = {};
+        const PokemonsFiltres = sourceDonnees.filter((pokemon) => sourceDonnees.form === "Normal");
+        PokemonsFiltres.forEach((pokemon) => {  
+            donneesFiltreesPokemon.push(PokemonsFiltres);
+        });
+        return donneesFiltreesPokemon;
+    }
+
     toString() {
-        const filteredPokemon = pokemon.filter((pokemon) => pokemon.form === "Normal");
+        // const filteredPokemon = pokemon.filter((pokemon) => pokemon.form === "Normal");
         filteredPokemon.forEach((pokemon) => {  
             console.log(`Pokemon: ${pokemon.pokemon_name} - ${pokemon.pokemon_id} - ${pokemon.form} - ${pokemon.base_attack} - ${pokemon.base_defense} - ${pokemon.base_stamina}`);
         });
     }
 }
+
 /*
 let pokemon1 = new Pokemon(1, "Bulbasaur", "Normal", 118, 111, 128);
 alert(pokemon1.toString());
 
-sourceDonnees = "https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json";
-
-
-alert(pokemon1.sourceDonnees);
-
-function importData(){
+*/
+/* Pour importer les données, faire un objectProperty et dire que all_pokemons est un objet static. Vérifier aussi la condition que la forme du pokemon soit = "Normal" (cf : code de Mini)*/
+/*function import_data {
     static allPokemons = {};
 
-    allPokemons = fetch(sourceDonnees);
+    static async importData(sourceImport) {
+        const sourceDonnees = sourceImport;
 
-
+        try {
+            const reponse = fetch(sourceDonnees);
+            const donnees = reponse.json();
+            if (Object.entries(this.allPokemons[3] === "Normal")){
+                this.allPokemons = donnees.pokemon;
+            }/*
+            this.allPokemons = donnees.pokemon.filter((pokemon) => pokemon.form === "Normal");
+            */
+           /*
+        } catch (erreurChargement) {
+            console.error("Une erreur s'est produite lors de l'importation des données :", erreurChargement);
+        }
+    }
 }
+*/
 
-/* Pour importer les données, faire un objectProperty et dire que all_pokemons est un objet static. Vérifier aussi la condition que la forme du pokemon soit = "Normal" (cf : code de Mini)*/
-
+// import_data.importData(/files/pokemon.js);
